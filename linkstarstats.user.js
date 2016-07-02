c021a018c3c083351546f6906dc5dc143d6a69d5
Hurqalia <Hurqalia@users.noreply.github.com>
2016-07-02 18:44:36 +0200

    Update linkstarstats.user.js

--- linkstarstats.user.js
+++ linkstarstats.user.js
@@ -2,12 +2,12 @@
 // @id             linkstarstats
 // @name           IITC plugin: Linkstart Stats
 // @category       Layer
-// @version        0.1.0.20160702.001
+// @version        0.1.2.20160702.002
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Hurqalia/linkstarstats/raw/master/linkstarstats.meta.js
 // @downloadURL    https://github.com/Hurqalia/linkstarstats/raw/master/linkstarstats.user.js
 // @installURL     https://github.com/Hurqalia/linkstarstats/raw/master/linkstarstats.user.js
-// @description    [linkstarstats-2016-07-02-001] LinkstarStats
+// @description    [linkstarstats-2016-07-02-002] LinkstarStats
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
@@ -18,7 +18,7 @@
 function wrapper(plugin_info) {
 	if(typeof window.plugin !== 'function') window.plugin = function() {};
 	plugin_info.buildName = 'hurqalia22';
-	plugin_info.dateTimeVersion = '20160423.005';
+	plugin_info.dateTimeVersion = '20160702.002';
 	plugin_info.pluginId = 'ontheroad';
 
 	// PLUGIN START ////////////////////////////////////////////////////////
