52b448679f3ac0072a9a42911cd6ecebb88cbf06
Hurqalia <Hurqalia@users.noreply.github.com>
2016-07-02 18:44:03 +0200

    Update linkstarstats.meta.js

--- linkstarstats.meta.js
+++ linkstarstats.meta.js
@@ -2,12 +2,12 @@
 // @id             linkstarstats
 // @name           IITC plugin: Linkstart Stats
 // @category       Layer
-// @version        0.1.0.20160702.001
+// @version        0.1.2.20160702.002
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Hurqalia/linkstarstats/raw/master/linkstarstats.meta.js
 // @downloadURL    https://github.com/Hurqalia/linkstarstats/raw/master/linkstarstats.user.js
 // @installURL     https://github.com/Hurqalia/linkstarstats/raw/master/linkstarstats.user.js
-// @description    [linkstarstats-2016-07-02-001] LinkstarStats
+// @description    [linkstarstats-2016-07-02-002] LinkstarStats
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
