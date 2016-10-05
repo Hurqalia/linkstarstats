037e17844522859ebfcfe8205b0b264c6622a1b6
Hurqalia <Hurqalia@users.noreply.github.com>
2016-10-05 13:31:09 +0200

    Update linkstarstats.meta.js
    Fix Niantics redirect

--- linkstarstats.meta.js
+++ linkstarstats.meta.js
@@ -2,15 +2,19 @@
 // @id             linkstarstats
 // @name           IITC plugin: Linkstart Stats
 // @category       Layer
-// @version        0.1.2.20160702.002
+// @version        0.1.3.20160702.002
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Hurqalia/linkstarstats/raw/master/linkstarstats.meta.js
 // @downloadURL    https://github.com/Hurqalia/linkstarstats/raw/master/linkstarstats.user.js
 // @installURL     https://github.com/Hurqalia/linkstarstats/raw/master/linkstarstats.user.js
 // @description    [linkstarstats-2016-07-02-002] LinkstarStats
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
